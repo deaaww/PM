af5e7107be351bfdbcea6d714ad44a41dfc3696b
deaaww <deamarsell12@gmail.com>
2026-04-23 20:53:14 +0700

    Lay out a widget (Material apps)

--- Week#06/src/basic_layout_flutter/README.md
+++ Week#06/src/basic_layout_flutter/README.md
@@ -33,3 +33,25 @@ class MyApp extends StatelessWidget {
 }
 ```
 ![](images/standard_apps.png)
+---
+Material apps
+```
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Flutter layout demo';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(appTitle)),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
+```
+![](images/material_apps.png)
--- Week#06/src/basic_layout_flutter/lib/main.dart
+++ Week#06/src/basic_layout_flutter/lib/main.dart
@@ -9,13 +9,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(color: Colors.white),
-      child: const Center(
-        child: Text(
-          'Hello World',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 32, color: Colors.black87),
+    const String appTitle = 'Flutter layout demo';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(appTitle)),
+        body: const Center(
+          child: Text('Hello World'),
         ),
       ),
     );
